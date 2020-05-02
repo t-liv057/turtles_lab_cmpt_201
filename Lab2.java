2571ebfdfb5349c247c55a8a8be29cc357362470
t-liv057 <taylorlivingston057@live.com>
2020-05-01 22:16:33 -0600

    Add files via upload

---
+++ Lab2.java
@@ -0,0 +1,54 @@
+/**
+ * Auto Generated Java Class.
+ */
+public class Lab2 {
+  
+  
+  public static void main(String[] args) { 
+/* 
+World world = new World();
+Turtle turtle1;
+turtle1 = new Turtle(world);
+System.out.println(turtle1);
+turtle1.forward(100);
+turtle1.turnRight();
+turtle1.forward(100);
+turtle1.turnRight();
+turtle1.forward(100);
+turtle1.turnRight();
+turtle1.forward(100);
+System.out.println(turtle1); */
+
+/*
+    World world; 
+Turtle turtle1, turtle2;   // can declare multiple variables if same type
+world = new World();
+turtle1 = new Turtle(world);
+turtle1.turn(45);
+turtle1.forward(120);
+turtle2 = new Turtle(60,60,world);
+turtle2.turn(135);
+turtle2.forward(240);
+turtle2.moveTo(404, 156);
+turtle2.turn(270);
+turtle1.moveTo(700, 500);
+System.out.println(turtle1);
+System.out.println(turtle2);
+*/
+    
+World world;             // declare variables
+Turtle turtle1; 
+int length; 
+world = new World();      // start creating objects
+turtle1 = new Turtle(world);
+turtle1.writeX(150);
+turtle1.penUp();
+turtle1.moveTo(200,200);
+turtle1.penDown();
+turtle1.turn(180);
+turtle1.writeV(150);
+  }
+  
+  /* ADD YOUR CODE HERE */
+  
+}
